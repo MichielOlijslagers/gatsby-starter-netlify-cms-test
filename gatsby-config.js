cb0c80b572b552b9614ba682bf5459e3773138ee
Michiel Olijslagers <info@michielolijslagers.com>
2021-03-07 12:24:35 +0100

    web

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    title: "My Gatsby Site",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "pEKnVLxQTsUc6-dcSMaEXnUf8xERsrLEH5Ek2zffeDk",
+        spaceId: "cyp10x6qrlqr",
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
